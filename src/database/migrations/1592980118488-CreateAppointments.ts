582ba947b65c16cdcbfce4db9285bfa11649dcd9
Mackdesign <marcio@design2u.com.br>
2020-06-24 05:04:28 -0300

    Connection Typeorm parte 1

--- src/database/migrations/1592980118488-CreateAppointments.ts
+++ src/database/migrations/1592980118488-CreateAppointments.ts
@@ -1,7 +1,36 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1592980118488 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+export default class CreateAppointments1592980118488
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            // type: 'timestamp', MYSQL
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
 }
