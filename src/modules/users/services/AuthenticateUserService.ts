844d3ae4ed6b38707a3e433c144013d772f97e51
Mackdesign <marcio@design2u.com.br>
2020-10-14 20:44:43 -0300

    Teste e TDD

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,17 +1,17 @@
-import { compare } from 'bcryptjs';
+// import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
   email: string;
   password: string;
-  token: string;
 }
 
 interface IResponse {
@@ -24,16 +24,22 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository, // eslint-disable-next-line no-empty-function
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider, // eslint-disable-next-line no-empty-function
   ) {}
 
-  public async execute({ email, password }: IRequest): Promise<Response> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
